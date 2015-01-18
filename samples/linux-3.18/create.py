2b4d37edb1cd4b057b44f414cd0e495e6f623dfd
Matthäus G. Chajdas <dev@anteru.net>
2015-01-18 20:18:26 +0100

    Add a large test installation.
    The provided script generates a test installation for four versions of the Linux
    kernel (2.5 GiB, 200k files total.)

---
+++ samples/linux-3.18/create.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from lxml import etree as ET
+import os
+import sys
+
+srcData = {
+	'Linux_3_18' : 'linux-3.18',
+	'Linux_3_18_1' : 'linux-3.18.1',
+	'Linux_3_18_2' : 'linux-3.18.2',
+	'Linux_3_18_3' : 'linux-3.18.3'
+}
+
+root = ET.Element ('Installer')
+product = ET.SubElement (root, 'Product')
+product.set ('Name', 'Linux-3.18')
+product.set ('Version', '3.18')
+
+features = ET.SubElement (product, 'Features')
+sourcePackages = ET.SubElement (product, 'SourcePackages')
+
+for key, value in sorted (srcData.items ()):
+	feature = ET.SubElement (features, 'Feature')
+	feature.set ('Id', 'Feature_{}'.format (key))
+	fileGroupRef = ET.SubElement (feature, 'FileGroupReference')
+	fileGroupRef.set ('Id', 'Files_{}'.format (key))
+
+	dataRoot = os.path.join (os.getcwd (), 'data', value)
+	fileGroup = ET.SubElement (product, 'FileGroup')
+	fileGroup.set ('Id', 'Files_{}'.format (key))
+	
+	for directory, _, entry in os.walk (dataRoot):
+		directory = directory [len (os.path.join (os.getcwd (), 'data')) + 1:]
+		if entry:
+			for e in entry:
+				fileElement = ET.SubElement (fileGroup, 'File')
+				fileElement.set ('Source', os.path.join (directory, e))
+				
+sys.stdout.write (ET.tostring (root, pretty_print=True).decode ('utf-8'))
---
+++ samples/linux-3.18/fetch.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.18.tar.xz
+dtrx linux-3.18.tar.xz
+mv linux-3.18  data
+
+wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.18.1.tar.xz
+dtrx linux-3.18.1.tar.xz
+mv linux-3.18.1 data
+
+wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.18.2.tar.xz
+dtrx linux-3.18.2.tar.xz
+mv linux-3.18.2 data
+
+wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.18.3.tar.xz
+dtrx linux-3.18.3.tar.xz
+mv linux-3.18.3 data
+
+rm *.tar.xz
