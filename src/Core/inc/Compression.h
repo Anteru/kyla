47ab98704deb837d9d3e5f01eb17f71ba574e98d
Matthäus G. Chajdas <dev@anteru.net>
2015-02-07 13:36:41 +0100

    More work on the compression streams.

--- src/Core/inc/Compression.h
+++ src/Core/inc/Compression.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <functional>
 #include <memory>
+#include <vector>
 
 namespace kyla {
 enum class CompressionMode : std::uint8_t
@@ -23,11 +24,15 @@ public:
 	void Initialize (std::function<void (const void* data, const std::int64_t)> writeCallback);
 	void Update (const void* data, const std::int64_t size);
 	void Finalize ();
+	void Compress (const void* data, const std::int64_t size,
+		std::vector<std::uint8_t>& buffer);
 
 private:
 	virtual void InitializeImpl (std::function<void (const void* data, const std::int64_t)> writeCallback) = 0;
 	virtual void UpdateImpl (const void* data, const std::int64_t size) = 0;
 	virtual void FinalizeImpl () = 0;
+	virtual void CompressImpl (const void* data, const std::int64_t size,
+		std::vector<std::uint8_t>& buffer);
 };
 
 class ZipStreamCompressor final : public StreamCompressor
@@ -40,9 +45,30 @@ private:
 	void InitializeImpl (std::function<void (const void* data, const std::int64_t)> writeCallback) override;
 	void UpdateImpl (const void* data, const std::int64_t size) override;
 	void FinalizeImpl () override;
+	void CompressImpl (const void* data, const std::int64_t size,
+		std::vector<std::uint8_t>& buffer) override;
 
 	struct Impl;
 	std::unique_ptr<Impl> impl_;
+};
+
+class UncompressedStreamCompressor final : public StreamCompressor
+{
+	void InitializeImpl (std::function<void (const void *, const std::int64_t)> writeCallback) override
+	{
+		writeCallback_ = writeCallback;
+	}
+
+	void UpdateImpl (const void *data, const std::int64_t size) override
+	{
+		writeCallback_ (data, size);
+	}
+
+	void FinalizeImpl () override
+	{
+	}
+
+	std::function<void (const void* data, const std::int64_t)> writeCallback_;
 };
 }
 
--- src/Core/src/Compression.cpp
+++ src/Core/src/Compression.cpp
@@ -5,6 +5,44 @@
 #include <vector>
 
 namespace kyla {
+////////////////////////////////////////////////////////////////////////////////
+void StreamCompressor::Initialize (std::function<void (const void* data, const std::int64_t)> writeCallback)
+{
+	InitializeImpl (writeCallback);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void StreamCompressor::Update (const void* data, const std::int64_t size)
+{
+	UpdateImpl (data, size);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void StreamCompressor::Finalize ()
+{
+	FinalizeImpl ();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void StreamCompressor::Compress (const void* data, const std::int64_t size,
+	std::vector<std::uint8_t>& buffer)
+{
+	CompressImpl (data, size, buffer);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void StreamCompressor::CompressImpl (const void* data, const std::int64_t size,
+	std::vector<std::uint8_t>& buffer)
+{
+	Initialize ([&](const void* data, const std::int64_t size) -> void {
+		buffer.insert (buffer.end (), static_cast<const std::uint8_t*> (data),
+									  static_cast<const std::uint8_t*> (data) + size);
+	});
+	Update (data, size);
+	Finalize ();
+}
+
+////////////////////////////////////////////////////////////////////////////////
 class ZipStreamCompressor::Impl
 {
 public:
@@ -30,27 +68,84 @@ public:
 		zStream_.avail_in = size;
 		zStream_.next_in = static_cast<Bytef*> (const_cast<void*> (data));
 
-		for (;;) {
-			zStream_.avail_out = buffer_.size ();
-			zStream_.next_out = buffer_.data ();
+		zStream_.avail_out = buffer_.size ();
+		zStream_.next_out = buffer_.data ();
 
+		do {
 			deflate (&zStream_, Z_NO_FLUSH);
-
-			// Finally, we didn't fill the output buffer
-			if (zStream_.avail_out != 0) {
-				break;
-			}
-		}
+			Write ();
+		} while (zStream_.avail_in > 0);
 	}
 
 	void Finalize ()
 	{
+		if (zStream_.next_in == nullptr) {
+			return;
+		}
+
+		int r = Z_OK;
+
+		do {
+			r = deflate (&zStream_, Z_FINISH);
+			Write ();
+		} while (r == Z_OK);
+
 		deflateEnd (&zStream_);
 	}
 
 private:
+	void Write ()
+	{
+		const auto written = buffer_.size () - zStream_.avail_out;
+		if (written > 0) {
+			callback_ (buffer_.data (), written);
+			zStream_.avail_out = buffer_.size ();
+			zStream_.next_out = buffer_.data ();
+		}
+	}
+
 	std::function<void (const void* data, const std::int64_t)> callback_;
 	std::vector<std::uint8_t> buffer_;
 	z_stream zStream_;
 };
+
+////////////////////////////////////////////////////////////////////////////////
+ZipStreamCompressor::ZipStreamCompressor ()
+	: impl_ (new Impl)
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+ZipStreamCompressor::~ZipStreamCompressor ()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ZipStreamCompressor::InitializeImpl (std::function<void (const void* data, const std::int64_t)> writeCallback)
+{
+	impl_->Initialize (writeCallback);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ZipStreamCompressor::UpdateImpl (const void* data, const std::int64_t size)
+{
+	impl_->Update (data, size);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ZipStreamCompressor::FinalizeImpl ()
+{
+	impl_->Finalize ();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ZipStreamCompressor::CompressImpl (const void* data, const std::int64_t size,
+	std::vector<std::uint8_t>& buffer)
+{
+	buffer.resize (::compressBound (size));
+
+	uLongf destLen = buffer.size ();
+	compress (buffer.data (), &destLen, static_cast<const Bytef*> (data), size);
+	buffer.resize (destLen);
+}
 }
