0506701f1794e5850f2249eed3499f1a9a9934bc
Matthäus G. Chajdas <dev@anteru.net>
2016-05-16 21:47:14 +0200

    Add a couple more tests and improve the test runner.

--- test/testrunner.py
+++ test/testrunner.py
@@ -5,6 +5,7 @@ import hashlib
 import json
 import tempfile
 from collections import OrderedDict
+import glob
 
 class KylaRunner:
     def __init__(self, kclBinaryPath):
@@ -25,8 +26,11 @@ class KylaRunner:
     def Install(self, source, target, filesets=[]):
         return self._ExecuteAction ('install', source, target, filesets) == 0
 
+    def Configure(self, source, target, filesets=[]):
+        return self._ExecuteAction ('configure', source, target, filesets) == 0
+
     def _ExecuteAction(self, action, source, target, filesets):
-        args = [self._kcl, action, source, target, ' '.join (filesets)]
+        args = [self._kcl, action, source, target] + filesets
 
         #TODO handle validate
         return subprocess.check_call (args)
@@ -57,17 +61,37 @@ class ExecuteInstall:
 
         return env.kyla.Install (source, target, filesets)
 
+class ExecuteConfigure:
+    def Execute(self, env : TestEnvironment, args):
+        source = os.path.join (env.testDirectory, args ['source'])
+        target = os.path.join (env.testDirectory, args ['target'])
+        filesets = args ['filesets']
+
+        return env.kyla.Configure (source, target, filesets)
+
 class CheckHash:
     def Execute (self, env : TestEnvironment, args):
         for k,v in args.items ():
-            if hashlib.sha256 (open (os.path.join (env.testDirectory, k), 'rb').read()).digest().hex() != v:
+            try:
+                if hashlib.sha256 (open (os.path.join (env.testDirectory, k), 'rb').read()).digest().hex() != v:
+                    return False
+            except:
+                return False
+        return True
+
+class CheckNotExistant:
+    def Execute (self, env : TestEnvironment, args):
+        for arg in args:
+            if os.path.exists (os.path.join (env.testDirectory, arg)):
                 return False
         return True
 
 hooks = {
     "generate-repository" : SetupGenerateRepository,
     "install" : ExecuteInstall,
-    "check-hash" : CheckHash
+    "configure" : ExecuteConfigure,
+    "check-hash" : CheckHash,
+    "check-not-existant" : CheckNotExistant
 }
 
 class TestRunner:
@@ -77,22 +101,28 @@ class TestRunner:
             object_pairs_hook=OrderedDict)
 
     def Execute(self):
-        print ('Executing test: ' + self.__test ['info']['description'])
         with tempfile.TemporaryDirectory () as tempDir:
-            print ('Working directory: "{}"'.format (tempDir))
             env = TestEnvironment (self.__kyla, tempDir)
 
             for phase in ['setup', 'execute', 'test']:
-                print ('Executing phase: ' + phase)
                 for step in self.__test [phase]:
                     k = list (step.keys ()) [0]
                     v = step [k]
                     hook = hooks [k] ()
                     r = hook.Execute (env, v)
-                    print ('Step: {}, pass: {}'.format (k, r))
+
+                    if r == False:
+                        return False
+        return True
 
 if __name__ == '__main__':
     kyla = r"F:\Build\kyla\bin\Debug\kcl.exe"
 
-    tr = TestRunner (KylaRunner (kyla), 'tests/basic_install.json')
-    tr.Execute ()
+    for test in glob.glob ('tests/*.json'):
+        testName = os.path.splitext (os.path.basename (test))[0]
+        tr = TestRunner (KylaRunner (kyla), test)
+        r = tr.Execute ()
+        if r:
+            print (testName, 'PASS')
+        else:
+            print (testName, 'FAIL')
