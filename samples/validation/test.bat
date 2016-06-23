9f889bd10f63b5cdeec6de0f801dd7143a24be11
Matthäus G. Chajdas <dev@anteru.net>
2016-06-23 21:19:01 +0200

    Remove the validation samples.

--- samples/validation/test.bat
+++
@@ -1,5 +0,0 @@
-rmdir /S /Q install v1-i v2-i
-kcl build --source-directory data-1 v-1.xml v1-i
-kcl build --source-directory data-2 v-2.xml v2-i
-kcl install v1-i install 23ba19b4-5162-41b3-8ff1-4d104604901e 25d05924-a2f9-488f-988d-40d6e64706c1
-kcl configure v2-i install b1b913b6-c903-48c0-88be-f4863a599484 d842ad8b-eaf2-4ae4-83fa-59fbe0939eb9
