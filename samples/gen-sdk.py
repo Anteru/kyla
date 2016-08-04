608f8171ec74c59d32ee3446ccd6519fecde59aa
Matthäus G. Chajdas <dev@anteru.net>
2016-08-04 15:07:23 +0200

    Add license to the bindings and deploy them in the SDK.

--- samples/gen-sdk.py
+++ samples/gen-sdk.py
@@ -13,25 +13,29 @@ if __name__ == '__main__':
     binaryDir = sys.argv [2]
 
     print ('Preparing directories', flush=True)
-    shutil.rmtree ('sdk-package', ignore_errors=True)
+    shutil.rmtree ('staging_sdk', ignore_errors=True)
     shutil.rmtree ('sdk', ignore_errors=True)
-    os.makedirs('sdk-package/samples/glfw')
-    os.makedirs('sdk-package/bin')
-    os.makedirs ('sdk-package/bin/platforms')
-    os.makedirs('sdk-package/include')
+
+    for folder in ['samples/glfw', 'bin', 'bin/platforms', 'include', 'python']:
+        os.makedirs(os.path.join ('staging_sdk', folder))
 
     print ('Copying docs', flush=True)
-    shutil.copytree (os.path.join (srcDir, 'doc/build/html'), 'sdk-package/docs')
-    shutil.copyfile (os.path.join (srcDir, 'src/kyla/inc/Kyla.h'), 'sdk-package/include/Kyla.h')
+    shutil.copytree (os.path.join (srcDir, 'doc/build/html'), 'staging_sdk/docs')
+    shutil.copyfile (os.path.join (srcDir, 'src/kyla/inc/Kyla.h'), 'staging_sdk/include/Kyla.h')
 
     print ('Generating samples', flush=True)
-    GenerateGLFWSamples ('sdk-package/samples/glfw')
+    GenerateGLFWSamples ('staging_sdk/samples/glfw')
 
     print ('Generating SDK installer', flush=True)
     sdk = FileRepositoryBuilder ()
 
     docs = sdk.AddFileSet ('docs')
-    docs.AddFilesFromDirectory ('sdk-package/docs', prefix='docs')
+    docs.AddFilesFromDirectory ('staging_sdk/docs', prefix='docs')
+
+    shutil.copyfile (os.path.join (srcDir, 'scripts/kyla.py'), 'staging_sdk/python/kyla.py')
+    pythonBindings = sdk.AddFileSet ('python-bindings')
+    pythonBindings.AddFilesFromDirectory ('staging_sdk/python', prefix='python')
+
     binaries = sdk.AddFileSet ('binaries')
 
     print ('Copying binaries', flush=True)
@@ -41,19 +45,19 @@ if __name__ == '__main__':
     ]
 
     for binary in binaryFiles:
-     shutil.copyfile (os.path.join (binaryDir, binary), 'sdk-package/bin/' + binary)
+     shutil.copyfile (os.path.join (binaryDir, binary), 'staging_sdk/bin/' + binary)
 
-    binaries.AddFilesFromDirectory ('sdk-package/bin', prefix='bin')
+    binaries.AddFilesFromDirectory ('staging_sdk/bin', prefix='bin')
     include_files = sdk.AddFileSet ('include_files')
-    include_files.AddFilesFromDirectory ('sdk-package/include', prefix='include')
+    include_files.AddFilesFromDirectory ('staging_sdk/include', prefix='include')
     samples = sdk.AddFileSet ('samples')
-    samples.AddFilesFromDirectory ('sdk-package/samples', prefix='samples')
+    samples.AddFilesFromDirectory ('staging_sdk/samples', prefix='samples')
     doc = sdk.Finalize ()
     open ('sdk.xml', 'w').write (doc)
 
     print ('Packaging installer', flush=True)
     subprocess.check_call ([os.path.join (binaryDir, 'kcl.exe'), 'build',
-        '--source-directory=sdk-package', 'sdk.xml', 'sdk'])
+        '--source-directory=staging_sdk', 'sdk.xml', 'sdk'])
 
-    print ('Cleaning up', flush=True)    
-    shutil.rmtree ('sdk-package', ignore_errors=True)
+    print ('Cleaning up', flush=True)
+    shutil.rmtree ('staging_sdk', ignore_errors=True)
