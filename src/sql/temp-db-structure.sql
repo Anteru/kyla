21693db408bc9e878f019e4cdf3c5a2be674e8e0
Matthäus G. Chajdas <dev@anteru.net>
2016-08-27 10:19:39 +0200

    Remove the temp-db-structure which wasn't used anywhere.

--- src/sql/temp-db-structure.sql
+++
@@ -1,6 +0,0 @@
-PRAGMA foreign_keys = ON;
-
-# This is used during configure to store the source content objects
-# Notice it's non-null, as we don't care about duplicates
-CREATE TABLE source_content_objects (
-    Hash BLOB NOT NULL);
