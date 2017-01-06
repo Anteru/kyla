dc2b53cb3556227383783b3e5628cbd6fe296715
Matthäus G. Chajdas <dev@anteru.net>
2017-01-06 19:47:50 +0100

    Add a couple of unit tests for the Hash class.

---
+++ src/base/test/Hash_test.cpp
@@ -0,0 +1,27 @@
+#include "Hash.h"
+
+#include <catch.h>
+
+TEST_CASE ("HashEqual", "[hash]")
+{
+	kyla::HashDigest<2> a, b;
+	a.bytes[0] = 0;
+	a.bytes[1] = 0;
+
+	b.bytes[0] = 1;
+	b.bytes[1] = 1;
+
+	REQUIRE (a != b);
+	REQUIRE (a == a);
+	REQUIRE (b == b);
+}
+
+TEST_CASE ("HashToString", "[hash]")
+{
+	kyla::HashDigest<2> a;
+	a.bytes[0] = 0xFA;
+	a.bytes[1] = 0xBC;
+
+	const auto s = ToString (a);
+	REQUIRE ("fabc" == s);
+}
